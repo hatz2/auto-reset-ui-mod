87ddfcc18732460779e62f983d608ac3a2898a0e
Hatz <alextorres4101@gmail.com>
2024-04-17 10:37:38 +0200

    Update dllmain.cpp

--- AutoResetUI/dllmain.cpp
+++ AutoResetUI/dllmain.cpp
@@ -76,7 +76,6 @@ void auto_reset_ui_mod() {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-        //CreateThread(NULL, 0, MainThread, hModule, 0, NULL);
         DisableThreadLibraryCalls(hModule);
         auto_reset_ui_mod();
         break;
